f9a051e3e315e046fc995b7e399188c19050d98b
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-11-07 23:21:15 +0000

    Add example nav

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,55 +3,33 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './../assets/sass/app.sass'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <header>
+    <nav>
+      <ul>
+        <li><Link to="/">Example</Link></li>
+        <li><Link to="/">Example</Link></li>
+        <li><Link to="/">Example</Link></li>
+        <li><Link to="/">Example</Link></li>
+        <li><Link to="/">Example</Link></li>
+      </ul>
+    </nav>
+  </header>
 )
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Daniel Griffiths | Web Developer"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>

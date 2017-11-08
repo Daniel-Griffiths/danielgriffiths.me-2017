8e84db55d0ee9a51ba21d396b0d3d0e598aed113
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2017-11-08 01:10:21 +0000

    Initial code for the blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
   	`gatsby-plugin-react-helmet`,
-  	`gatsby-plugin-stylus`
+  	`gatsby-transformer-remark`,
+  	`gatsby-plugin-stylus`,
+  	{
+  		resolve: `gatsby-source-filesystem`,
+  		options: {
+  			path: `${__dirname}/src/pages`,
+  			name: `pages` 
+  		}
+  	}
   ],
 }
---
+++ src/pages/blog/08-11-2017/index.md
@@ -0,0 +1,8 @@
+---
+path: '/first-post'
+title: 'First Post'
+---
+
+# Test Blog Post
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio aliquid, ipsam accusantium id veniam, nam corporis delectus explicabo repellat similique consequatur assumenda numquam, cupiditate ea at alias quam deleniti tempora!
